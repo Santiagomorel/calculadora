98d63075396c2dcaae29e57b7403452458cc85e3
usuario <usuario@NBVL006.argentum.com.ar>
2022-11-04 17:43:26 -0300

    division y potencia

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,17 @@
 class Calculadora:
-    def __init__(self, numero1, numero2):
-        pass
+    def multiplicacion(numero1,numero2):
+        try:
+            int(numero1)
+            int(numero2)
+            print(numero1*numero2)
+        except ValueError:
+            print("ERROR valor no valido")
+    def potencia(numero1,numero2):
+        try:
+            int(numero1)
+            int(numero2)
+            print(numero1**numero2)
+        except ValueError:
+             print("ERROR valor no valido")
+calculadora = Calculadora()
+Calculadora.multiplicacion(2,3)
