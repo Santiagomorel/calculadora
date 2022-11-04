cc5a543b2720bebb1c1ab271ce9cf1bc39f22dca
Santiagomorel <smorel@frba.utn.edu.ar>
2022-11-04 17:32:14 -0300

    agrego suma y resta

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,21 @@
 class Calculadora:
-    def __init__(self, numero1, numero2):
-        self.numero1 = numero1
-        self.numero2 = numero2
+    def suma(self, numero1, numero2):
+        try:
+            int(numero1)
+            int(numero2)
+            print(numero1 + numero2)
+        except ValueError:
+            print("No se puede realizar la suma porque no se pasaron numeros como valor")
+    
+    def resta(self, numero1, numero2):
+        try:
+            int(numero1)
+            int(numero2)
+            print(numero1 - numero2)
+        except ValueError:
+            print("No se puede realizar la resta porque no se pasaron numeros como valor")
+
+
+calculadora = Calculadora()
+calculadora.suma(1,2)
+calculadora.resta(3,4)
